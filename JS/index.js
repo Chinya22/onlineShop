299e38732edd0a2b9a55b9235058caffe2b4f57c
Chinya22 <anastasia220899@gmail.com>
2024-06-11 15:01:37 +0300

    динамическое добавление карточек из массива с помощью JS

--- JS/index.js
+++ JS/index.js
@@ -1,176 +1,176 @@
-const cardsList = document.querySelector('.main_container_list');
+const cardsInfoList = [
+  {
+    title: "Вертикальный велотренажер B37",
+    img: "img/trainer.png",
+    price: "1134900 ₽",
+    oldPrice: "34900 ₽",
+    characteristics: [
+      {
+        name: "Тип дорожки:",
+        value: "Домашняя",
+      },
+      {
+        name: "Бег. полотно:",
+        value: "1200 х 450 мм",
+      },
+      {
+        name: "Мощность двигателя:",
+        value: "2,0 л.с.",
+      },
+      {
+        name: "Беговое полотно:",
+        value: "2-х слойное",
+      },
+      {
+        name: "Производитель:",
+        value: "Cardio Power",
+      },
+    ],
+  },
+  {
+    title: "Вертикальный велотренажер B37",
+    img: "img/trainer.png",
+    price: "1134900 ₽",
+    oldPrice: "34900 ₽",
+    characteristics: [
+      {
+        name: "Тип дорожки:",
+        value: "Домашняя",
+      },
+      {
+        name: "Бег. полотно:",
+        value: "1200 х 450 мм",
+      },
+      {
+        name: "Мощность двигателя:",
+        value: "2,0 л.с.",
+      },
+      {
+        name: "Беговое полотно:",
+        value: "2-х слойное",
+      },
+      {
+        name: "Производитель:",
+        value: "Cardio Power",
+      },
+    ],
+  }
+];
 
-const card = document.createElement('div');
-card.classList.add('main_container_list_item');
-cardsList.append(card);
+for (let i = 0; i < cardsInfoList.length; i++) {
+    createCard(cardsInfoList[i], '.main_container_list')
+}
 
-const img = document.createElement('img');
-img.src = 'img/trainer.png';
-img.classList.add('main_container_list_item_img');
-card.prepend(img);
- 
-const percentWrapper = document.createElement('div');
-percentWrapper.classList.add ('main_container_list_item_percent');
-const percentImg = document.createElement('img');
-percentImg.src = 'img/percent.svg';
-percentImg.classList.add('main_container_list_item_percent_img');
-percentWrapper.prepend(percentImg);
-card.prepend(percentWrapper);
+function createCard(cardInfo, cardsWrapper) {
+  const cardsList = document.querySelector(cardsWrapper);
 
-const likeWrapper = document.createElement('div');
-likeWrapper.classList.add ('main_container_list_item_like');
-const likeImg = document.createElement('img');
-likeImg.src = 'img/like.svg';
-likeImg.classList.add('main_container_list_item_like_img');
-likeWrapper.prepend(likeImg);
-card.prepend(likeWrapper);
+  const card = document.createElement("div");
+  card.classList.add("main_container_list_item");
+  cardsList.append(card);
 
-const newWrapper = document.createElement('div');
-newWrapper.classList.add ('main_container_list_item_new');
-const newImg = document.createElement('img');
-newImg.src = 'img/NEW.svg';
-newImg.classList.add('main_container_list_item_new_img');
-newWrapper.prepend(newImg);
-card.prepend(newWrapper);
+  const img = document.createElement("img");
+  img.src = cardInfo.img;
+  img.classList.add("main_container_list_item_img");
+  card.prepend(img);
 
-const stick = document.createElement('img');
-stick.src = 'img/stick.svg';
-stick.classList.add('main_container_list_item_statistics');
-card.append(stick); 
+  const percentWrapper = document.createElement("div");
+  percentWrapper.classList.add("main_container_list_item_percent");
+  const percentImg = document.createElement("img");
+  percentImg.src = "img/percent.svg";
+  percentImg.classList.add("main_container_list_item_percent_img");
+  percentWrapper.prepend(percentImg);
+  card.prepend(percentWrapper);
 
-const heart = document.createElement('img');
-heart.src = 'img/heart_card.svg';
-heart.classList.add('main_container_list_item_heart');
-card.append(heart);
+  const likeWrapper = document.createElement("div");
+  likeWrapper.classList.add("main_container_list_item_like");
+  const likeImg = document.createElement("img");
+  likeImg.src = "img/like.svg";
+  likeImg.classList.add("main_container_list_item_like_img");
+  likeWrapper.prepend(likeImg);
+  card.prepend(likeWrapper);
 
-const info = document.createElement('div');
-info.classList.add ('main_container_list_item_info');
-card.append(info);
+  const newWrapper = document.createElement("div");
+  newWrapper.classList.add("main_container_list_item_new");
+  const newImg = document.createElement("img");
+  newImg.src = "img/NEW.svg";
+  newImg.classList.add("main_container_list_item_new_img");
+  newWrapper.prepend(newImg);
+  card.prepend(newWrapper);
 
+  const stick = document.createElement("img");
+  stick.src = "img/stick.svg";
+  stick.classList.add("main_container_list_item_statistics");
+  card.append(stick);
 
-const presence = document.createElement('div');
-presence.classList.add ('main_container_list_item_info_presence');
-presence.innerText = 'Есть в шоу-руме';
-info.prepend(presence);
+  const heart = document.createElement("img");
+  heart.src = "img/heart_card.svg";
+  heart.classList.add("main_container_list_item_heart");
+  card.append(heart);
 
-const presencePoints = document.createElement('img');
-presencePoints.src = 'img/points.svg';
-presencePoints.classList.add ('main_container_list_item_info_presence_points');
-presence.prepend(presencePoints);
+  const info = document.createElement("div");
+  info.classList.add("main_container_list_item_info");
+  card.append(info);
 
-const presenceTextWrapper = document.createElement('span');
-presenceTextWrapper.innerText = 'В наличии';
-presence.prepend(presenceTextWrapper);
+  const presence = document.createElement("div");
+  presence.classList.add("main_container_list_item_info_presence");
+  presence.innerText = "Есть в шоу-руме";
+  info.prepend(presence);
 
-const titleCard = document.createElement('h3');
-titleCard.classList.add ('main_container_list_item_info_title');
-titleCard.innerText = 'Вертикальный велотренажер B37';
-info.append(titleCard);
+  const presencePoints = document.createElement("img");
+  presencePoints.src = "img/points.svg";
+  presencePoints.classList.add("main_container_list_item_info_presence_points");
+  presence.prepend(presencePoints);
 
-const type = document.createElement('div');
-type.classList.add('main_container_list_item_info_type');
-type.innerText = 'Домашняя';
-info.append(type);
+  const presenceTextWrapper = document.createElement("span");
+  presenceTextWrapper.innerText = "В наличии";
+  presence.prepend(presenceTextWrapper);
 
-const typeSpan = document.createElement('span');
-typeSpan.innerText = 'Тип дорожки: ';
-type.prepend(typeSpan);
+  const titleCard = document.createElement("h3");
+  titleCard.classList.add("main_container_list_item_info_title");
+  titleCard.innerText = cardInfo.title;
+  info.append(titleCard);
 
+  for (let i = 0; i < cardInfo.characteristics.length; i++) {
+    const type = document.createElement("div");
+    type.classList.add("main_container_list_item_info_item");
+    type.innerText = cardInfo.characteristics[i].value;
+    info.append(type);
 
-const size = document.createElement('div');
-size.classList.add('main_container_list_item_info_size');
-size.innerText = '1200 х 450 мм';
-info.append(size);
+    const typeSpan = document.createElement("span");
+    typeSpan.innerText = cardInfo.characteristics[i].name + " ";
+    type.prepend(typeSpan);
+  }
 
-const sizeSpan = document.createElement('span');
-sizeSpan.innerText = 'Бег. полотно: ';
-size.prepend(sizeSpan);
+  const rating = document.createElement("div");
+  rating.classList.add("main_container_list_item_info_rating");
+  rating.innerText = "Рейтинг ";
+  info.append(rating);
 
+  const stars = document.createElement("img");
+  stars.src = "img/stars.svg";
+  stars.classList.add("main_container_list_item_info_rating_img");
+  rating.append(stars);
 
-const power = document.createElement('div');
-power.classList.add('main_container_list_item_info_power');
-power.innerText = '2,0 л.с.';
-info.append(power);
+  const buy = document.createElement("div");
+  buy.classList.add("main_container_list_item_info_buy");
+  info.append(buy);
 
-const powerSpan = document.createElement('span');
-powerSpan.innerText = 'Мощность двигателя: ';
-power.prepend(powerSpan);
+  const buyPrice = document.createElement("div");
+  buyPrice.classList.add("main_container_list_item_info_buy_price");
+  buyPrice.innerText = "от " + cardInfo.price;
+  buy.append(buyPrice);
 
+  const buyPriceSpan = document.createElement("span");
+  buyPriceSpan.innerText = cardInfo.oldPrice;
+  buyPrice.append(buyPriceSpan);
 
-const canvas = document.createElement('div');
-canvas.classList.add('main_container_list_item_info_canvas');
-canvas.innerText = '2-х слойное';
-info.append(canvas);
+  const buyBtn = document.createElement("button");
+  buyBtn.classList.add("main_container_list_item_info_buy_btn");
+  buyBtn.innerText = "Купить";
+  buy.append(buyBtn);
 
-const canvasSpan = document.createElement('span');
-canvasSpan.innerText = 'Беговое полотно: ';
-canvas.prepend(canvasSpan);
-
-
-const manufacturer = document.createElement('div');
-manufacturer.classList.add('main_container_list_item_info_manufacturer');
-manufacturer.innerText = 'Cardio Power';
-info.append(manufacturer);
-
-const manufacturerSpan = document.createElement('span');
-manufacturerSpan.innerText = 'Производитель ';
-manufacturer.prepend(manufacturerSpan);
-
-
-const rating = document.createElement('div');
-rating.classList.add('main_container_list_item_info_rating');
-rating.innerText = 'Рейтинг ';
-info.append(rating);
-
-const stars = document.createElement('img');
-stars.src = 'img/stars.svg';
-stars.classList.add('main_container_list_item_info_rating_img');
-rating.append(stars);
-
-
-const buy = document.createElement('div');
-buy.classList.add('main_container_list_item_info_buy');
-info.append(buy);
-
-const buyPrice = document.createElement('div');
-buyPrice.classList.add('main_container_list_item_info_buy_price');
-buyPrice.innerText = 'от 1 134 900 ₽';
-buy.append(buyPrice);
-
-const buyPriceSpan = document.createElement('span');
-buyPriceSpan.innerText = '34 900 ₽';
-buyPrice.append(buyPriceSpan);
-
-
-const buyBtn = document.createElement('button');
-buyBtn.classList.add('main_container_list_item_info_buy_btn');
-buyBtn.innerText = 'Купить';
-buy.append(buyBtn);
-
-const buyBtnImg = document.createElement('img');
-buyBtnImg.src = 'img/bag_card.svg';
-buyBtnImg.classList.add('main_container_list_item_info_buy_btn_img');
-buyBtn.prepend(buyBtnImg);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  const buyBtnImg = document.createElement("img");
+  buyBtnImg.src = "img/bag_card.svg";
+  buyBtnImg.classList.add("main_container_list_item_info_buy_btn_img");
+  buyBtn.prepend(buyBtnImg);
+}
